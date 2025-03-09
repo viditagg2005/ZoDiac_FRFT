b78140869ee0be73c000d9824e40b5fe527e7ff9
viditagg2005 <viditaggarwal1152005@gmail.com>
2025-03-09 20:21:39 +0530

    updated loss & config

--- loss/loss.py
+++ loss/loss.py
@@ -6,6 +6,10 @@ import torch
 import torch.nn as nn
 from .watson_vgg import WatsonDistanceVgg
 from .pytorch_ssim import SSIM
+import frft # make sure to include the frft file
+import yaml
+with open('./example/config/config.yaml', 'r') as file:
+    cfgs = yaml.safe_load(file)
 
 class LossProvider(nn.Module):
     def __init__(self, loss_weights: list, device):
@@ -22,8 +26,8 @@ class LossProvider(nn.Module):
         self.loss_per = lambda pred_img, gt_img: loss_percep((1+pred_img)/2.0, (1+gt_img)/2.0)/ pred_img.shape[0]
 
     def __call__(self, pred_img_tensor, gt_img_tensor, init_latents, wm_pipe):
-        init_latents_fft = torch.fft.fftshift(torch.fft.fft2(init_latents), dim=(-1, -2))
-        lossW = self.loss_w(init_latents_fft[wm_pipe.watermarking_mask], wm_pipe.gt_patch[wm_pipe.watermarking_mask])*self.loss_weights[3]
+        init_latents_frft = torch.fft.fftshift(frft.frft2d(init_latents, order = cfgs['order'] )) # add the order here (IMPORTANT!!!!!)
+        lossW = self.loss_w(init_latents_frft[wm_pipe.watermarking_mask], wm_pipe.gt_patch[wm_pipe.watermarking_mask])*self.loss_weights[3]
         lossI = self.loss_img(pred_img_tensor, gt_img_tensor)*self.loss_weights[0]
         lossP = self.loss_per(pred_img_tensor, gt_img_tensor)*self.loss_weights[1]
         lossS = (1-self.loss_ssim(pred_img_tensor, gt_img_tensor))*self.loss_weights[2]
